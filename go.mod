46b22d1cd4cdd9ed278b158c61fc7df1af2f7b8f
fanyang <fanyang89@outlook.com>
2025-05-29 10:22:54 +0800

    Add dedup

--- go.mod
+++ go.mod
@@ -3,9 +3,20 @@ module github.com/fanyang89/gofd
 go 1.24.3
 
 require (
+	github.com/cespare/xxhash v1.1.0
 	github.com/gobwas/glob v0.2.3
+	github.com/opencontainers/selinux v1.12.0
+	github.com/schollz/progressbar/v3 v3.18.0
+	github.com/syndtr/goleveldb v1.0.0
 	github.com/urfave/cli/v3 v3.3.3
 	go.uber.org/zap v1.27.0
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+)
--- gofd.go
+++ gofd.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"crypto/sha1"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"time"
 
+	"github.com/cespare/xxhash"
 	"github.com/gobwas/glob"
+	"github.com/opencontainers/selinux/pkg/pwalkdir"
+	"github.com/schollz/progressbar/v3"
+	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/urfave/cli/v3"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 type Action interface {
@@ -74,6 +86,7 @@ var cmd = &cli.Command{
 	Usage: "fd in go",
 	Arguments: []cli.Argument{
 		&cli.StringArg{Name: "path"},
+		&cli.StringArg{Name: "path2", Value: ""},
 	},
 	Flags: []cli.Flag{
 		&cli.StringFlag{
@@ -92,8 +105,18 @@ var cmd = &cli.Command{
 	},
 	Action: func(ctx context.Context, command *cli.Command) error {
 		path := command.StringArg("path")
-		action := newAction(command.String("action"))
-		mode := newSearchType(command.String("type"))
+		path2 := command.StringArg("path2")
+		actionStr := command.String("action")
+
+		if actionStr == "dedup" {
+			if path2 == "" {
+				return errors.New("please specify another path for deduplicate")
+			}
+			return deduplicate(path, path2)
+		}
+
+		action := newAction(actionStr)
+		searchMode := newSearchType(command.String("type"))
 
 		globStr := command.String("glob")
 		matcher, err := glob.Compile(globStr)
@@ -106,7 +129,7 @@ var cmd = &cli.Command{
 				return nil
 			}
 
-			switch mode {
+			switch searchMode {
 			case onlyFile:
 				if info.IsDir() {
 					return nil
@@ -144,12 +167,159 @@ var cmd = &cli.Command{
 	},
 }
 
+type multiHash struct {
+	XXHash uint64
+	SHA1   [3]uint64
+}
+
+func sha1ToArray(buf []byte) [3]uint64 {
+	if len(buf) != 20 {
+		panic(fmt.Errorf("invalid buffer, len: %d", len(buf)))
+	}
+	a := [3]uint64{}
+	a[0] = binary.BigEndian.Uint64(buf[0:8])
+	a[1] = binary.BigEndian.Uint64(buf[8:16])
+	a[2] = uint64(binary.BigEndian.Uint32(buf[16:20]))
+	return a
+}
+
+func hashFile(path string) (multiHash, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return multiHash{}, err
+	}
+	defer func() { _ = f.Close() }()
+
+	reader := bufio.NewReader(f)
+	x := xxhash.New()
+	s := sha1.New()
+
+	buf := make([]byte, 1<<20)
+	for {
+		var n int
+		n, err = reader.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return multiHash{}, err
+		}
+
+		s.Write(buf[:n])
+		_, err = x.Write(buf[:n])
+		if err != nil {
+			return multiHash{}, err
+		}
+	}
+
+	return multiHash{
+		XXHash: x.Sum64(),
+		SHA1:   sha1ToArray(s.Sum(nil)),
+	}, nil
+}
+
+func (h *multiHash) Bytes() []byte {
+	buf := make([]byte, 32)
+	binary.BigEndian.PutUint64(buf[:8], h.XXHash)
+	binary.BigEndian.PutUint64(buf[8:16], h.SHA1[0])
+	binary.BigEndian.PutUint64(buf[16:24], h.SHA1[1])
+	binary.BigEndian.PutUint64(buf[24:32], h.SHA1[2])
+	return buf
+}
+
+func createHashMap(path string, db *leveldb.DB) error {
+	bar := progressbar.NewOptions(-1,
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowCount(),
+		progressbar.OptionThrottle(time.Second),
+	)
+	bar.Describe(fmt.Sprintf("Creating deduplicate hash map, path: %s", path))
+	defer func() { _ = bar.Finish() }()
+
+	return pwalkdir.Walk(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+
+		h, err := hashFile(path)
+		if err != nil {
+			return err
+		}
+
+		return db.Put(h.Bytes(), []byte(path), nil)
+	})
+}
+
+func deduplicate(path1 string, path2 string) error {
+	dbPath1, err := os.MkdirTemp("", "gofd-")
+	if err != nil {
+		return err
+	}
+
+	db1, err := leveldb.OpenFile(dbPath1, nil)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = db1.Close() }()
+
+	err = createHashMap(path1, db1)
+	if err != nil {
+		return err
+	}
+
+	dbPath2, err := os.MkdirTemp("", "gofd-")
+	if err != nil {
+		return err
+	}
+
+	db2, err := leveldb.OpenFile(dbPath2, nil)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = db1.Close() }()
+
+	err = createHashMap(path2, db2)
+	if err != nil {
+		return err
+	}
+
+	iter := db2.NewIterator(nil, nil)
+	for iter.Next() {
+		key := iter.Key()
+		path := iter.Value()
+
+		_, err = db1.Get(key, nil)
+		if err != nil {
+			if errors.Is(err, leveldb.ErrNotFound) {
+				continue
+			} else {
+				return err
+			}
+		} else {
+			p := string(path)
+			zap.L().Info("Removing file", zap.String("path", p))
+			_ = os.Remove(p)
+		}
+	}
+	err = iter.Error()
+	iter.Release()
+	return err
+}
+
 func main() {
-	logger, _ := zap.NewDevelopment(zap.AddCaller(), zap.AddCallerSkip(1))
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logger, err := config.Build(zap.AddCaller(), zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
 	zap.ReplaceGlobals(logger)
 	defer func() { _ = logger.Sync() }()
 
-	err := cmd.Run(context.Background(), os.Args)
+	err = cmd.Run(context.Background(), os.Args)
 	if err != nil {
 		zap.L().Error("Unexpected error", zap.Error(err))
 	}
